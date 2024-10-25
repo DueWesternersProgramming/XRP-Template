e48bf2480760640eaca10e205dca632e61e6e04c
Due Westerners <frc-programmers@duewestrobotics.org>
2024-10-24 20:11:23 -0400

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+public final class Constants {
+
+    public static final class ControllerConstants{
+        public static final int DRIVER_CONTROLLER_PORT = 0;
+        public static final int LEFT_SIDE_JOYSICK_AXIS = 1;
+        public static final int RIGHT_SIDE_JOYSICK_AXIS = 5;
+    }
+}
