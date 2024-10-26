f4bb092b76d3f512294d626958b77e902178cf64
hjmcphail76 <hjmcphail76@gmail.com>
2024-10-26 15:29:34 -0400

    kinda follows the path, I need to tune values.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,46 +8,69 @@ import frc.robot.Constants.ControllerConstants;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.TeleopDriveCommand;
 import frc.robot.subsystems.DriveSubsystem;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DriveSubsystem driveSubsystem = new DriveSubsystem();
   private final Joystick driverJoystick = new Joystick(ControllerConstants.DRIVER_CONTROLLER_PORT);
-  private final ExampleCommand autoCommand = new ExampleCommand(driveSubsystem);
+  private SendableChooser<Command> m_autoChooser = new SendableChooser<>();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     driveSubsystem.setDefaultCommand(new TeleopDriveCommand(driveSubsystem, driverJoystick));
-
+    configureAutos();
     configureButtonBindings();
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+   * subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    
+
   }
-    
+
+  private void configureAutos() {// Setup SmartDashboard options
+    // m_chooser.setDefaultOption("Auto Routine Distance", new
+    // AutonomousDistance(m_drivetrain));
+    // m_chooser.addOption("Auto Routine Time", new
+    // AutonomousDistance(m_drivetrain));
+    m_autoChooser = AutoBuilder.buildAutoChooser();
+    m_autoChooser.addOption("ExampleAuto", new ExampleCommand(driveSubsystem));
+    SmartDashboard.putData(m_autoChooser);
+  }
+
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return autoCommand;
+    return m_autoChooser.getSelected() != null ? m_autoChooser.getSelected() : driveSubsystem.resetGyroAngle();
   }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,22 +4,41 @@
 
 package frc.robot.subsystems;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.xrp.XRPGyro;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.CowboyUtils;
 
 public class DriveSubsystem extends SubsystemBase {
-  private static final double kGearRatio =
-      (30.0 / 14.0) * (28.0 / 16.0) * (36.0 / 9.0) * (26.0 / 8.0); // 48.75:1
+  private static final double kGearRatio = (30.0 / 14.0) * (28.0 / 16.0) * (36.0 / 9.0) * (26.0 / 8.0); // 48.75:1
   private static final double kCountsPerMotorShaftRev = 12.0;
   private static final double kCountsPerRevolution = kCountsPerMotorShaftRev * kGearRatio; // 585.0
   private static final double kWheelDiameterInch = 2.3622; // 60 mm
-
+  private static final double kTrackWidthInch = 6.1; // 60 mm
+  private static final double kMaxSpeedMetersPerSecond = 0.15;
   // The XRP has the left and right motors set to
   // channels 0 and 1 respectively
   private final XRPMotor m_leftMotor = new XRPMotor(0);
   private final XRPMotor m_rightMotor = new XRPMotor(1);
+
+  private final XRPGyro m_gyro = new XRPGyro();
 
   // The XRP has onboard encoders that are hardcoded
   // to use DIO pins 4/5 and 6/7 for the left and right
@@ -27,8 +46,14 @@ public class DriveSubsystem extends SubsystemBase {
   private final Encoder m_rightEncoder = new Encoder(6, 7);
 
   // Set up the differential drive controller
-  private final DifferentialDrive m_diffDrive =
-      new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
+  private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
+
+  private final DifferentialDriveKinematics m_kinematics = new DifferentialDriveKinematics(
+      CowboyUtils.inchesToMeters(kTrackWidthInch));
+
+  private final DifferentialDriveOdometry odometry;
+  private final Field2d m_field2d = new Field2d();
+  SysIdRoutine routine;
 
   /** Creates a new XRPDrivetrain. */
   public DriveSubsystem() {
@@ -36,19 +61,53 @@ public class DriveSubsystem extends SubsystemBase {
     m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
     m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
     resetEncoders();
+    m_gyro.reset();
 
     // Invert right side since motor is flipped
     m_rightMotor.setInverted(true);
+
+    odometry = new DifferentialDriveOdometry(getGyroAngleRotation2d(),
+        CowboyUtils.inchesToMeters(getLeftDistanceInch()),
+        CowboyUtils.inchesToMeters(getRightDistanceInch()));
+    AutoBuilder.configureRamsete(this::getRobotPose, this::resetOdometry, this::getChassisSpeeds,
+        this::driveChassisSpeeds, -1, 1,
+        new ReplanningConfig(true, false),
+        () -> false, this);
+
   }
 
   public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
     m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
   }
 
-  public void tankDrive(double leftSpeed, double rightSpeed){
+  public void tankDrive(double leftSpeed, double rightSpeed) {
     m_diffDrive.tankDrive(leftSpeed, rightSpeed);
   }
 
+  public void driveChassisSpeeds(ChassisSpeeds speeds) {
+    arcadeDriveSpeedsMetersPerSec(speeds.vxMetersPerSecond, speeds.omegaRadiansPerSecond);
+  }
+
+  public void arcadeDriveSpeedsMetersPerSec(double x, double rot) {
+    // Convert from meters per second to percentage of max speed (-1.0 to 1.0)
+    double xPercent = x / kMaxSpeedMetersPerSecond;
+    double rotPercent = rot / kMaxSpeedMetersPerSecond;
+
+    // Ensure the values stay within the range of -1.0 to 1.0
+    xPercent = Math.max(Math.min(xPercent, 1.0), -1.0);
+    rotPercent = Math.max(Math.min(rotPercent, 1.0), -1.0);
+
+    // Use arcadeDrive to apply the converted percentages
+    m_diffDrive.arcadeDrive(xPercent, rotPercent);
+  }
+
+  public ChassisSpeeds getChassisSpeeds() {
+    return m_kinematics.toChassisSpeeds(getWheelSpeeds());
+  }
+
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(), m_rightEncoder.getRate());
+  }
 
   public void resetEncoders() {
     m_leftEncoder.reset();
@@ -63,14 +122,51 @@ public class DriveSubsystem extends SubsystemBase {
     return m_rightEncoder.getDistance();
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public double getGyroAngleDegrees() {
+    return m_gyro.getAngleZ();
+  }
+
+  public Rotation2d getGyroAngleRotation2d() {
+    return new Rotation2d(Math.toRadians(getGyroAngleDegrees()));
+  }
+
+  private void updateOdometry() {
+    odometry.update(getGyroAngleRotation2d(),
+        getWheelPositionsMeters());
+    m_field2d.setRobotPose(getRobotPose());
+  }
+
+  public void resetGyro() {
+    m_gyro.reset();
+  }
+
+  public Pose2d getRobotPose() {
+    return odometry.getPoseMeters();
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    odometry.resetPosition(getGyroAngleRotation2d(), getWheelPositionsMeters(), pose);
+  }
+
+  public DifferentialDriveWheelPositions getWheelPositionsMeters() {
+    return new DifferentialDriveWheelPositions(CowboyUtils.inchesToMeters(getLeftDistanceInch()),
+        CowboyUtils.inchesToMeters(getRightDistanceInch()));
   }
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    // This method will be called once per scheduler run, every 20 ms.
+    updateOdometry();
     m_diffDrive.feed();
+    SmartDashboard.putNumber("Gyro Angle", getGyroAngleDegrees());
+    SmartDashboard.putData(m_field2d);
   }
+
+  public Command resetGyroAngle() {
+    return new StartEndCommand(() -> {
+      resetGyro();
+    }, () -> {
+    });
+  }
+
 }
